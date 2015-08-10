23735a44778587fa6ef341cb56b86dc96ae604b6
Diego Prats <diego.prats@gmail.com>
2015-08-09 20:49:08 -0700

    create simple express server

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
-console.log('Hi Krystal');
+var express = require('express');
+var app = express();
+
+app.use('/', express.static('public'));
+
+app.get('/', function(req, res){
+  // res.send('hello world');
+  res.sendFile('public/index.html');
+});
+
+app.listen(3000);
+
+console.log('listeng on port 3000');
